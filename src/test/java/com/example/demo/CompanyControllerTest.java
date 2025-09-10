33d2d322e22fd67e8dbb223391dd43e2f8dba45c
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-10 10:48:31 +0800

    refactor: should_return_company_when_get_id_found

--- src/test/java/com/example/demo/CompanyControllerTest.java
+++ src/test/java/com/example/demo/CompanyControllerTest.java
@@ -63,20 +63,18 @@ public class CompanyControllerTest {
                 .andExpect(jsonPath("$.length()").value(1));
     }
 
-//    @Test
-//    void should_return_company_when_get_id_found() throws Exception {
-//        Company spring = new Company();
-//        spring.setName("Spring");
-//        Company company = companyController.createCompany(spring);
-//
-//        MockHttpServletRequestBuilder request = get("/companies/" + company.getId())
-//                .contentType(MediaType.APPLICATION_JSON);
-//        mockMvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(company.getId()))
-//                .andExpect(jsonPath("$.name").value(company.getName()));
-//    }
-//
+    @Test
+    void should_return_company_when_get_id_found() throws Exception {
+        createSpring();
+
+        MockHttpServletRequestBuilder request = get("/companies/" + 1)
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("spring"));
+    }
+
 //    @Test
 //    void should_return_company_when_put_with_id_found() throws Exception {
 //        Company spring = new Company();
