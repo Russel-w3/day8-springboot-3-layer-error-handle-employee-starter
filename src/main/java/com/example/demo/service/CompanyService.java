6e0f4ed5fd9714d6cdfccc1202e2a235df03f274
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-10 17:33:43 +0800

    refactor: remove unused import

--- src/main/java/com/example/demo/service/CompanyService.java
+++ src/main/java/com/example/demo/service/CompanyService.java
@@ -2,14 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Company;
 import com.example.demo.repository.CompanyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
