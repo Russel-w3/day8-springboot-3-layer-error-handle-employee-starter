0fb7030b157f008e8bba948179315b6e2b2ea2d4
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-10 17:23:39 +0800

    test: add should_return_employee_when_create_an_employee for CompanyServiceTest

---
+++ src/test/java/com/example/demo/CompanyServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import com.example.demo.entity.Company;
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.CompanyRepository;
+import com.example.demo.service.CompanyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class CompanyServiceTest {
+
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Mock
+    private CompanyRepository companyRepository;
+
+    @Test
+    void should_return_employee_when_create_an_employee() {
+        Company company = new Company(null,"oracle");
+        when(companyRepository.createCompany(any())).thenReturn(company);
+        Company companyResult = companyService.createCompany(company);
+        assertEquals(company.getName(), companyResult.getName());
+    }
+}
