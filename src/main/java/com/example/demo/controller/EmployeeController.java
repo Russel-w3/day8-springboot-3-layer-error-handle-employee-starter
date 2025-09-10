017a201cce926df0d863ebe2b9090848583703c7
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-10 11:44:16 +0800

    refactor: remove unused variable

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,13 +5,11 @@ import com.example.demo.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    private final List<Employee> employees = new ArrayList<>();
 
     private final EmployeeService employeeService;
 
