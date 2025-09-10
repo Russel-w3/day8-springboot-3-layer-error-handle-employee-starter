144b9edbd166f030e796b4adcf432bd337ce58ba
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-10 10:49:44 +0800

    refactor: should_return_company_when_put_with_id_found

--- src/test/java/com/example/demo/CompanyControllerTest.java
+++ src/test/java/com/example/demo/CompanyControllerTest.java
@@ -75,26 +75,24 @@ public class CompanyControllerTest {
                 .andExpect(jsonPath("$.name").value("spring"));
     }
 
-//    @Test
-//    void should_return_company_when_put_with_id_found() throws Exception {
-//        Company spring = new Company();
-//        spring.setName("Spring");
-//        Company company = companyController.createCompany(spring);
-//        String requestBody = """
-//                {
-//                    "name": "Spring2"
-//                }
-//                """;
-//        MockHttpServletRequestBuilder request = put("/companies/" + company.getId())
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(requestBody);
-//
-//        mockMvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(company.getId()))
-//                .andExpect(jsonPath("$.name").value("Spring2"));
-//    }
-//
+    @Test
+    void should_return_company_when_put_with_id_found() throws Exception {
+        createSpring();
+        String requestBody = """
+                {
+                    "name": "spring2"
+                }
+                """;
+        MockHttpServletRequestBuilder request = put("/companies/" + 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("spring2"));
+    }
+
 //    @Test
 //    void should_return_no_content_when_delete_id_found() throws Exception {
 //        Company spring = new Company();
