81a432cc45aed5fe7b2e4e6b891664b912607e56
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-11 13:48:25 +0800

    feat: update method cleanCompanies

--- src/test/java/com/example/demo/CompanyControllerTest.java
+++ src/test/java/com/example/demo/CompanyControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
@@ -21,9 +22,13 @@ public class CompanyControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @BeforeEach
     void cleanCompanies() throws Exception {
-        mockMvc.perform(delete("/companies/delete"));
+        jdbcTemplate.execute("DELETE FROM company");
+        jdbcTemplate.execute("ALTER TABLE company AUTO_INCREMENT = 1;");
     }
 
     private void createSpring() throws Exception {
