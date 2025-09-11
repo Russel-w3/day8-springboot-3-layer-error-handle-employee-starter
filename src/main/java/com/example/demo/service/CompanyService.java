aed936ed8e1ac6345f1d89bda73516de3f4a44cc
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-11 13:29:20 +0800

    feat: change the datasource and update method getCompanies

--- src/main/java/com/example/demo/service/CompanyService.java
+++ src/main/java/com/example/demo/service/CompanyService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Company;
-import com.example.demo.repository.CompanyRepository;
+import com.example.demo.repository.ICompanyRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,45 +13,50 @@ import java.util.List;
 @Service
 public class CompanyService {
 
-    private final CompanyRepository companyRepository;
+    private final ICompanyRepository companyRepository;
 
-    public CompanyService(CompanyRepository companyRepository) {
+    public CompanyService(ICompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
     }
 
     public List<Company> getCompanies(Integer page, Integer size) {
-        return companyRepository.getCompanies(page, size);
-    }
-
-    public Company createCompany(Company company) {
-        return companyRepository.createCompany(company);
-    }
-
-    public Company updateCompany(int id, Company updatedCompany) {
-        Company companyById = companyRepository.getCompanyById(id);
-        if (companyById == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found with id: " + id);
+        if (page == null || size == null) {
+            return companyRepository.findAll();
+        } else {
+            Pageable pageable = PageRequest.of(page - 1, size);
+            return companyRepository.findAll(pageable).stream().toList();
         }
-        return companyRepository.updateCompany(id, updatedCompany);
     }
 
-    public Company getCompanyById(int id) {
-        Company companyById = companyRepository.getCompanyById(id);
-        if (companyById == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found with id: " + id);
-        }
-        return companyById;
-    }
-
-    public void deleteCompany( int id) {
-        Company companyById = companyRepository.getCompanyById(id);
-        if (companyById == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found with id: " + id);
-        }
-        companyRepository.deleteCompany(id);
-    }
-
-    public void deleteAll(){
-        companyRepository.deleteAll();
-    }
+//    public Company createCompany(Company company) {
+//        return companyRepository.createCompany(company);
+//    }
+//
+//    public Company updateCompany(int id, Company updatedCompany) {
+//        Company companyById = companyRepository.getCompanyById(id);
+//        if (companyById == null) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found with id: " + id);
+//        }
+//        return companyRepository.updateCompany(id, updatedCompany);
+//    }
+//
+//    public Company getCompanyById(int id) {
+//        Company companyById = companyRepository.getCompanyById(id);
+//        if (companyById == null) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found with id: " + id);
+//        }
+//        return companyById;
+//    }
+//
+//    public void deleteCompany(int id) {
+//        Company companyById = companyRepository.getCompanyById(id);
+//        if (companyById == null) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found with id: " + id);
+//        }
+//        companyRepository.deleteCompany(id);
+//    }
+//
+//    public void deleteAll() {
+//        companyRepository.deleteAll();
+//    }
 }
